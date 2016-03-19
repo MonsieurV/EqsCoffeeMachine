1a1de4a8676ff120d2a418a101974a79e0702400
Yoan Tournade <yoan@ytotech.com>
2016-03-19 23:19:58 +0100

    Check if the machine is on or off

--- arduino.ino
+++ arduino.ino
@@ -15,12 +15,16 @@ int PIN_POWER = 2;
 int PIN_MAKE_SHORT = 3;
 // Wired to the Senseo Long Coffee push button.
 int PIN_MAKE_LONG = 4;
+// Senseo LED pin.
+int LED_PIN = A0;
 // Wired to the RPi, so it can order a short coffee.
 int PIN_ORDER_SHORT = 5;
 // Wired to the RPi, so it can order a short coffee.
 int PIN_ORDER_LONG = 6;
 int is_short_ordered;
 int is_long_ordered;
+int i;
+long sum;
 
 void setup() {
   Serial.begin(9600);
@@ -39,11 +43,24 @@ void loop() {
   is_short_ordered = digitalRead(PIN_ORDER_SHORT);
   is_long_ordered = digitalRead(PIN_ORDER_LONG);
   if(is_short_ordered || is_long_ordered) {
-    Serial.println("Powering the Senseo machine");
-    digitalWrite(PIN_POWER, LOW); 
-    delay(PUSH_BTN_DELAY);
-    digitalWrite(PIN_POWER, HIGH);
-    delay(40000);
+    Serial.println("Check if the Senseo machine is on");
+    sum = 0;
+    for(i = 0; i < 100; i++) {
+      sum += analogRead(LED_PIN);
+      delay(20);
+    }
+    // 37323 when hot.
+    // 18719 when default (not water).
+    // 35164 when heating.
+    Serial.println(sum);
+    if(sum < 10000) {
+      Serial.println("Powering the Senseo machine");
+      digitalWrite(PIN_POWER, LOW);
+      delay(PUSH_BTN_DELAY);
+      digitalWrite(PIN_POWER, HIGH);
+    }
+    Serial.println("Wait for the machine to be ready");
+    delay(50000);
     if(is_short_ordered) {
       Serial.println("Start a short coffee");
       digitalWrite(PIN_MAKE_SHORT, LOW);
@@ -56,7 +73,7 @@ void loop() {
       digitalWrite(PIN_MAKE_LONG, HIGH);
     }
     Serial.println("Wait for the coffee to be ready");
-    delay(30000);
+    delay(40000);
     Serial.println("Shut down");
     digitalWrite(PIN_POWER, LOW); 
     delay(PUSH_BTN_DELAY);
