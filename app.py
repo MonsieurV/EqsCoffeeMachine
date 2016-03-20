3481c81b1299d3e70bb25c9f9b475f0e7c32bb66
Cyril Fougeray <cyril.fougeray@gmail.com>
2016-03-20 16:29:03 +0100

    Coffee counter

--- app.py
+++ app.py
@@ -23,6 +23,8 @@ COFFEE_LONG = ['long']
 PIN_SHORT = 26
 PIN_LONG = 21
 
+coffee_counter = {}
+
 # Init GPIO.
 GPIO.setmode(GPIO.BCM)
 GPIO.setup(PIN_SHORT, GPIO.OUT)
@@ -43,6 +45,18 @@ def isShort(message):
 
 def isLong(message):
 	return any(a in message for a in COFFEE_LONG)
+
+def addCoffeeForUser(user, short_long):
+	if user not in coffee_counter:
+		coffee_counter[user] = {}
+		coffee_counter[user]['short'] = 0
+		coffee_counter[user]['long'] = 0
+		talk("First coffee for @" + user + "! Thumbs up from the HackEQS team! :+1:")
+
+	coffee_counter[user][short_long] = coffee_counter[user][short_long] + 1
+
+	if coffee_counter[user][short_long] % 10 == 0:
+		talk("Woow, that's your " + coffee_counter[user][short_long] + "th " + short_long + " coffee!")
 
 print('Listening to Slack...')
 try:
@@ -66,12 +80,14 @@ try:
 				time.sleep(1)
 				GPIO.output(PIN_SHORT, GPIO.LOW)
 				talk("Your short coffee is ready @" + user['user']['name'] + ". Short but strong, enjoy it!")
+				addCoffeeForUser(user['user']['name'], 'short')
 			if isLong(message):
 				talk('A long coffee for a long day: that\'s on the way!')
 				GPIO.output(PIN_LONG, GPIO.HIGH)
 				time.sleep(1)
 				GPIO.output(PIN_LONG, GPIO.LOW)
-				talk("Your long coffee is ready @" + user['user']['name'] + ". As long as your d**k, enjoy it!")
+				talk("Your long coffee is ready @" + user['user']['name'] + ". Enjoy it!")
+				addCoffeeForUser(user['user']['name'], 'long')
 		time.sleep(1)
 finally:
 	GPIO.cleanup()
