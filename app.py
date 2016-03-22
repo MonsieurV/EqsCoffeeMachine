3bfe6158d846e01745642ddd090426c239f4dbe4
Yoan Tournade <yoan@ytotech.com>
2016-03-22 14:45:28 +0100

    Store coffee stats in a json file to persist it

--- app.py
+++ app.py
@@ -14,6 +14,7 @@ TODO
 * Get technical state of machine from Slack: ready, default, heating
 * Fill tank 60 seconds after a default
 * If there are 2 succesive default throw an alert: "No more water?"
+* Produce a video of a typical day at Equisense with the Eqs Coffee Machine.
 """
 import time
 from slackclient import SlackClient
@@ -38,6 +39,13 @@ try:
 		PROCESSED_MESSAGES = json.load(f)
 except IOError:
 	pass
+FILE_STATS = 'stats.json'
+COFFEE_STATS = {}
+try:
+	with open(FILE_STATS) as f:
+		COFFEE_STATS = json.load(f)
+except IOError:
+	pass
 # Pin setup constants.
 # Push delay to simulate a push button action.
 PUSH_BTN_DELAY = 0.5
@@ -54,8 +62,6 @@ PIN_LED_SENSEO = 23
 PIN_MOISTURE_SENSOR = 24
 # Wired to the relay for the water pump.
 PIN_WATER_PUMP = 25
-
-coffee_counter = {}
 
 # Init GPIO.
 GPIO.setmode(GPIO.BCM)
@@ -132,16 +138,27 @@ def isLongInMessage(message):
 	return any(a in message for a in COFFEE_LONG)
 
 def addCoffeeForUser(user, short_long):
-	if user not in coffee_counter:
-		coffee_counter[user] = {}
-		coffee_counter[user]['short'] = 0
-		coffee_counter[user]['long'] = 0
-		talk("First coffee for @" + user + "! Thumbs up from the HackEQS team! :+1:")
-
-	coffee_counter[user][short_long] = coffee_counter[user][short_long] + 1
-
-	if coffee_counter[user][short_long] % 10 == 0:
-		talk("Woow, that's your " + coffee_counter[user][short_long] + "th " + short_long + " coffee!")
+	# Init the dict.
+	if 'short' not in COFFEE_STATS:
+		COFFEE_STATS['short'] = {}
+		COFFEE_STATS['short']['total'] = {}
+	if 'long' not in COFFEE_STATS:
+		COFFEE_STATS['long'] = {}
+		COFFEE_STATS['long']['total'] = {}
+	if user not in COFFEE_STATS:
+		COFFEE_STATS[user] = {}
+		COFFEE_STATS[user]['short'] = 0
+		COFFEE_STATS[user]['long'] = 0
+		talk("First coffee for @{0}! Thumbs up from the HackEQS team! :+1:"
+			.format(user))
+	# Update the dict.
+	COFFEE_STATS[short_long]['total'] = COFFEE_STATS[short_long]['total'] + 1
+	COFFEE_STATS[user][short_long] = COFFEE_STATS[user][short_long] + 1
+	if COFFEE_STATS[user][short_long] % 10 == 0:
+		talk("Woow, that's your {0}th {0} coffee!".
+			format(COFFEE_STATS[user][short_long], short_long))
+	with open(FILE_STATS, 'wb') as f:
+		json.dump(COFFEE_STATS, f)
 
 def setMessageAsProcessed(messageId):
 	PROCESSED_MESSAGES.append(messageId)
@@ -177,11 +194,9 @@ try:
 			isShort = isShortInMessage(message)
 			if isShort:
 				talk('One short coffee ordered!', channel=notification['channel'])
-				addCoffeeForUser(user['name'], 'short')
 			else:
 				talk('A long coffee for a long day: that\'s on the way!',
 					channel=notification['channel'])
-				addCoffeeForUser(user['name'], 'long')
 			# Start the coffee making sequence.
 			if getSenseoState() == 'default':
 				talk('Problem Ouston! Where\'s the water?',
@@ -216,15 +231,17 @@ try:
 				time.sleep(22)
 				talk("Your coffee is ready @{0}. :coffee: Short but strong, enjoy it!"
 					.format(user['name']), channel=notification['channel'])
+				addCoffeeForUser(user['name'], 'short')
 			else:
 				time.sleep(40)
 				talk("Your long coffee is ready @{0}. As long as your d**k, enjoy it!"
 					.format(user['name']), channel=notification['channel'])
+				addCoffeeForUser(user['name'], 'long')
 			print('Shut down the Senseo machine')
 			powerSenseo()
 		# If the tank is not full, activate the water pump.
 		GPIO.output(PIN_WATER_PUMP, GPIO.input(PIN_MOISTURE_SENSOR))
-		time.sleep(1)
+		time.sleep(0.5)
 finally:
 	GPIO.output(PIN_WATER_PUMP, GPIO.LOW)
 	GPIO.cleanup()
